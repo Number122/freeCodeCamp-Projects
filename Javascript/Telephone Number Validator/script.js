03bf11ba00bbffefa8a51f63daaa4786617fdc9c
Number122 <73112439+Number122@users.noreply.github.com>
2025-01-06 21:23:04 +0330

    create script.js

--- Javascript/Telephone Number Validator/script.js
+++ Javascript/Telephone Number Validator/script.js
@@ -1,1 +1,28 @@
-// I dont understand this so imma just leave it here for now
+const userInput = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+
+checkBtn.addEventListener("click", () => {
+  const phoneNumber = userInput.value;
+const regex = /^(1\s)?(\(\d{3}\)|\d{3})([-.\s]?)(\d{3})\3(\d{4})$/; 
+if (!phoneNumber.startsWith('1')) {
+    resultsDiv.innerHTML += `<p class="invalid">Number must start with 1: ${phoneNumber}</p>`;
+    return;
+} else if (regex.test(phoneNumber)) {
+    resultsDiv.innerHTML += `<p class="valid">Valid US number: ${phoneNumber}</p>`;
+  } else {
+    resultsDiv.innerHTML += `<p class="invalid">Invalid US number: ${phoneNumber}</p>`;
+  }
+});
+
+userInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      checkBtn.click(); 
+    }
+  });
+12
+clearBtn.addEventListener("click", () => {
+    resultsDiv.innerHTML = "";
+    userInput.value = "";
+});
